d63af8b1ee30da4ebc4d8de078b612bfcc3fe726
이태준 <itaejun@itaejuncBookPro.kornet>
2021-12-26 15:53:54 +0900

    yaho

---
+++ one/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num = 100;
+    cout << "hello put your number :" << endl;
+    cin >> num;
+    cout << "Your Number is : " << num << endl;
+    return 0;   
+}
